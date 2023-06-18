36fb9ea5289b5abf4b05eb12f3b128d2f8ba5e3c
Shaleen Sharma <shaleen.sharma@signitysolutions.com>
2023-06-18 15:36:46 +0530

    changes for home page

---
+++ just-app/src/app/content-area/content-area.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-area',
+  templateUrl: './content-area.component.html',
+  styleUrls: ['./content-area.component.css']
+})
+export class ContentAreaComponent {
+
+  postdata : any[] = [
+    {
+      "name":"Shaleen",
+      "username": "shaleentastic",
+      "caption":"this is my first post",
+      "time":"23h",
+      "imgUrl":"./assets/img/vickyaali.jpg"
+    },
+     {
+      "name":"Vikrant",
+      "username": "gautam69",
+      "caption":"this is my infinte post",
+      "time":"13h",
+      "imgUrl":"./assets/img/camila.jpeg"
+    },
+     {
+      "name":"ram",
+      "username": "ramatastic",
+      "caption":"this is my new post",
+      "time":"2h",
+      "imgUrl":"./assets/img/ana.jpeg"
+    },
+     {
+      "name":"saniya",
+      "username": "saniyatastic",
+      "caption":"this is my repeat post",
+      "time":"3h",
+      "imgUrl":"./assets/img/zarameri.jpg"
+    }
+  ];
+}
