ab7fd03bdb65428bdd77156895fdb67b7144c365
Shaleen Sharma <shaleen.sharma@signitysolutions.com>
2023-06-20 23:51:59 +0530

    new signup component

--- just-app/src/app/app-routing.module.ts
+++ just-app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomepageComponent },
- 
+ { path: 'signup', component: SignUpComponent },
 ];
 
 
